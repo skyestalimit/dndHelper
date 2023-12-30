9fb3a4f9bd6a506addcfe827985cf6eeddd4e03c
SkyEstaLimit <skyestalimit@gmail.com>
2023-12-30 15:38:03 -0500

    Code cleanup

--- helper.go
+++ helper.go
@@ -31,7 +31,6 @@ func parseRollArgs(rollArgs []string) []roller.DiceRoll {
 	// Parse the dice roll and return the ammount and size of dice
 	diceRolls := make([]roller.DiceRoll, 0)
 	for i := 0; i < len(rollArgs); i++ {
-		fmt.Println("Evaluating roll argument: ", rollArgs[i])
 		diceRoll, err := parseRollArg(rollArgs[i])
 		if err != nil {
 			fmt.Println(err)
@@ -51,19 +50,9 @@ func parseRollArg(rollArg string) (roller.DiceRoll, error) {
 	}
 	// Parse a single roll argument and returns it as a DiceRoll
 	rollArg, modifier := evaluateModifier(rollArg)
-	
-	argSlices := strings.Split(rollArg, "d")
-	dice, diceErr := strconv.Atoi(argSlices[0])
-	if diceErr != nil {
-		fmt.Println(fmt.Sprintf("Error converting dice value of %s: %s", argSlices, diceErr.Error()))
-		dice = 0
-	}
-	ammount, ammountErr := strconv.Atoi(argSlices[1])
-	if ammountErr != nil {
-		fmt.Println(fmt.Sprintf("Error converting ammount value of %s: %s", argSlices, ammountErr.Error()))
-		ammount = 0
-	}
-	return roller.DiceRoll{dice, ammount, modifier}, nil
+	diceSize, diceAmmount := evaluateDiceSizeAndAmmount(rollArg)
+
+	return roller.DiceRoll {diceSize, diceAmmount, modifier}, nil
 }
 
 func evaluateModifier(rollArg string) (string, int) {
@@ -90,6 +79,21 @@ func parseModifier(rollArg string, symbol string) (string, int){
 		}
 	}
 	return rollArg, mod
+}
+
+func evaluateDiceSizeAndAmmount(rollArg string) (int, int){
+	argSlices := strings.Split(rollArg, "d")
+	dice, diceErr := strconv.Atoi(argSlices[0])
+	if diceErr != nil {
+		fmt.Println(fmt.Sprintf("Error converting dice value of %s: %s", argSlices, diceErr.Error()))
+		dice = 0
+	}
+	ammount, ammountErr := strconv.Atoi(argSlices[1])
+	if ammountErr != nil {
+		fmt.Println(fmt.Sprintf("Error converting ammount value of %s: %s", argSlices, ammountErr.Error()))
+		ammount = 0
+	}
+	return dice, ammount
 }
 
 func printUsage() {
