4b3c8e95f5386acebfa54679e5997f09b7849683
skyestalimit <skyestalimit@gmail.com>
2024-01-09 23:42:11 -0500

    upgrade to diceroller v0.1.3

--- dndhelper.go
+++ dndhelper.go
@@ -54,6 +54,9 @@ func main() {
 		fmt.Println(rollsResult[i].String())
 	}
 	fmt.Println("Rolls sum:", diceroller.DiceRollResultsSum(rollsResult))
+
+	result, _ := diceroller.NewDiceRoll(2, 6, 4).PerformRollAndSum()
+	fmt.Println("One more: ", result)
 }
 
 func printUsage() {
